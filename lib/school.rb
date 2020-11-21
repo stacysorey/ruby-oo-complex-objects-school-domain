eac4b1f69424fec20faed8ad76435f46af967d75
stacysorey <stacysorey@gmail.com>
2020-11-21 00:01:15 -0500

    commit tree 5cced015f4f558eb62ed939aca625018d477b338

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
+require "pry"
 # code here!
+class School
+    attr_accessor :school_name, :roster, :student_name, :grade
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new{ |h, k| h[k] = [] }
+    end
+    
+    def add_student(student_name, grade)
+      @roster[grade] << student_name
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map {|k,v| v.sort!}
+        @roster
+    end
+end
